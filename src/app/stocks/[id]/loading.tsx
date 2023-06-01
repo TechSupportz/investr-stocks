cd2b66fea8f0d5716f474edf5fa4bf4795a5809e
Nitish <79391172+TechSupportz@users.noreply.github.com>
2023-06-02 01:52:00 +0800

    ✨ added separate `loading.tsx` for stocks page

---
+++ src/app/stocks/[id]/loading.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { Card} from "@tremor/react"
+
+function StocksLoading() {
+    return (
+        <div className="flex h-full animate-pulse gap-6">
+            <div className="flex h-full w-[70%] flex-col gap-6">
+                <Card className="flex h-[20%] items-center gap-8 bg-slate-200"></Card>
+                <Card className="h-[55%] bg-slate-200"></Card>
+                <div className="flex h-[25%] gap-6">
+                    <Card className="bg-slate-200" />
+                    <Card className="bg-slate-200" />
+                </div>
+            </div>
+            <div className="flex h-full w-[30%] flex-col gap-6">
+                <Card className="h-3/5 bg-slate-200" />
+                <Card className="h-2/5 bg-slate-200" />
+            </div>
+        </div>
+    )
+}
+
+export default StocksLoading
